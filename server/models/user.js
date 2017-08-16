b08749292652d0732b42fd08190fa0dae581e8f6
dlewis2017 <dlewfaith@gmail.com>
2017-08-16 14:13:39 -0400

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,28 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+
+UserSchema.statics.findByCredentials = function (email,password) {
+  var User = this;
+  //check if user exists
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    //check for token match
+    return new Promise((resolve,reject) => {
+      bcrypt.compare(password,user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 //code run before (pre) UserSchema -- for middle-ware
 //runs if password has been changed
 UserSchema.pre('save', function (next) {
--- server/server.js
+++ server/server.js
@@ -125,6 +125,21 @@ app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
 });
 
+/* Login */
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email','password']);
+  var user = new User(body);
+
+  //find user to check if valid
+  User.findByCredentials(body.email,body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((error) => {
+    res.status(400).send();
+  });
+});
+
 /* port */
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
